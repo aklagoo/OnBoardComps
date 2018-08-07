d6bb09cdb2aff96d89d64eaa78221aec90bc548e
Archan Lagoo <aklagoo@gmail.com>
2018-08-07 08:16:04 +0530

    Update main_ino.cpp

--- main_ino.cpp
+++ main_ino.cpp
@@ -2,26 +2,29 @@
 //Includes
 #include<Wire.h>
 #include<beacon.h>		//https://github.com/markfickett/arduinomorse
-#include<sleep.h>		//Arduino safe_sleep - Either use interrupts or library
+#include<sleep.h>			//Arduino safe_sleep - Either use interrupts or library
 #include<EEPROM.h>		//For reading EEPROM values
+#include<RTClib.h>		//https://github.com/adafruit/RTClib/blob/master/examples/ds3231/ds3231.ino
 
 //Defines
 
 //EEPROM flags
-#define E_START			0
+#define E_START				0
 #define E_DEPLOYED		2
 #define E_DETUMBLED		4
 #define E_HANDSHAKE		6
+#define E_TIME				8
 
 //Pins
-#define P_DEPLOY		2
-#define P_FMT			3
-#define P_KILL			4
+#define P_DEPLOY			2
+#define P_FMT					3
+#define P_KILL				4
+#define P_FMT_SWITCH	5
 #define P_ADC_SWITCH	7
 #define P_PAY_SWITCH	8
-#define P_RF_TX			11
-#define P_RF_RX			12
-#define P_RESET			13
+#define P_RF_TX				11
+#define P_RF_RX				12
+#define P_RESET				13
 #define P_BAT_LEVEL		A2
 #define P_TEMP_LM35		A3
 
@@ -38,8 +41,8 @@
 #define S_DEPLOY		10000
 
 //I2C addresses
-#define A_ADC			0;
-#define A_PAY			1;
+#define A_ADC			0
+#define A_PAY			1
 
 //Battery constants
 #define B_VOLTAGE		10
@@ -52,8 +55,12 @@
 //Commands
 #define C_ADC_DETUMBLE	0
 
+//Lengths
+#define L_BEACON_VALUES	10
+
 //Global variables
-beacon Beacon;
+beacon Beacon(P_FMT, P_RF_TX);
+RTC_DS3231 rtc;
 
 //===========================SECTION 2===========================
 void setup(){
@@ -79,7 +86,7 @@ void loop(){
 		safe_sleep(S_HANDSHAKE_DEL);
 	}
 	if(homeposition()){
-		Beacon.official(M_NORMAL);
+		Beacon.official(getBeaconValues());    //Add all parts here
 	}
 	Beacon.upl_chkrun();
 	watchdog();
@@ -102,11 +109,9 @@ void init(){
 	pinMode(P_RF_RX, OUTPUT);
 	pinMode(P_RESET, OUTPUT);
 
-	//Init I2C
+	//Init I2C and RTC
 	Wire.begin();
-
-	//Set pins for FM and ASK
-	Beacon.setPins(P_FMT, P_RF_TX);
+	rtc.begin();
 }
 
 void watchdog(){
@@ -147,10 +152,26 @@ void detumble(){
 		EEPROM.write(Wire.read());
 	}
 }
-/*
-TEMP
-BATT_PERC
-RESTARTS
-LAST COMMAND TIME AND LAST COMMAND
+
+uint8_t *getBeaconValues(){
+	uint8_t values[L_BEACON_VALUES];
+	values[0] = (uint8_t)((analogRead(P_TEMP_LM35)*(5.0/1024.0))*100);
+	values[1] = (uint8_t)((analogRead(P_BAT_LEVEL)*(5.0/1023.0)*100)/B_VOLTAGE);
+	values[2] = (uint8_t)(EEPROM.read(E_START)-1);
+	values[3] = (uint8_t)(EEPROM.read(E_TIME));
+	values[4] = (uint8_t)(EEPROM.read(E_TIME)+1);
+	values[5] = (uint8_t)(EEPROM.read(E_TIME)+2);
+	values[6] = (uint8_t)(EEPROM.read(E_TIME)+3);
+	values[7] = (uint8_t)(EEPROM.read(E_TIME)+4);
+	values[8] = (uint8_t)(EEPROM.read(E_TIME)+5);
+	values[9] = (uint8_t)(EEPROM.read(E_TIME)+6);
+	return values;
+}
+/*                                  BYTES
+TEMP                                1
+BATT_PERC                           1
+RESTARTS                            1
+LAST COMMAND TIME AND LAST COMMAND  2+1+*5=7, 1
 MEM_VALUE
+TOTAL = 10/11 Bytes
 */
